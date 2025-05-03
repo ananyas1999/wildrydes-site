ad8c0beffb3beee309b4b4cfcf8b714370626012
Ananya Srinath Achar <52341450+ananyas1999@users.noreply.github.com>
2025-05-02 20:37:24 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_VNWHwLAUB', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '1tj0c490p4ta7mqqm107ibem05', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
